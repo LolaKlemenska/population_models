da49cc0eafb1ad8bf9f38e816adc5cd23a3b8122
LolaKlemenska <153238130+LolaKlemenska@users.noreply.github.com>
2024-01-21 20:18:52 +0100

    Add files via upload

---
+++ zad1.projekt3.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+K = 100000  
+r = 0.4 
+t0 = 75  
+x0 = 5  
+
+def gompertz_eq(t, x):
+    return r * x * np.log(K / x)
+
+def euler(h, t, x):
+    for i in range(1, len(t)):
+        x[i] = x[i - 1] + h * gompertz_eq(t[i - 1], x[i - 1])
+    return x
+
+h = 1
+t = np.arange(t0, t0 + 50, h)
+
+x_gompertz = np.zeros(len(t))
+x_gompertz[0] = x0
+
+x_gompertz = euler(h, t, x_gompertz)
+
+x_verhulst = K / (1 + ((K / x0) - 1) * np.exp(-r * (t - t0)))
+
+plt.plot(t, x_gompertz, label='Gompertz')
+plt.plot(t, x_verhulst, label='Verhulst')
+plt.xlabel('Czas')
+plt.ylabel('Objętość guza')
+plt.legend()
+plt.show()
